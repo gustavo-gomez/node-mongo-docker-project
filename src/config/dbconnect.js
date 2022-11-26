349618d5780cd10973113bd358a0fe960a183a50
Aditya Shahare <shahare.aditya@gmail.com>
2022-11-26 13:39:04 +0530

    Added: Docker config for web app

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
-const { MONGO_URI } = process.env;
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 exports.connect = () => {
   mongoose
-    .connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    .connect(
+      `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    )
     .then(console.log('DB CONNECTION SUCCESSFUL'))
     .catch((err) => {
       console.error('FAILED TO CONNECT TO DB');
